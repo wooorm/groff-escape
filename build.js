4c8d377ea85c742b27be6e82ae278cff9d8c88a5
Titus Wormer <tituswormer@gmail.com>
2022-11-20 10:35:58 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -2,70 +2,67 @@
  * @typedef {import('tar').ReadEntry} ReadEntry
  */
 
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import path from 'node:path'
 import https from 'node:https'
 import tar from 'tar'
 import concatStream from 'concat-stream'
-import {bail} from 'bail'
 
 // To update, see <https://ftp.gnu.org/gnu/groff/> if there are newer versions.
 https
   .request('https://ftp.gnu.org/gnu/groff/groff-1.22.4.tar.gz', (response) => {
     response.pipe(tar.t()).on('entry', (/** @type {ReadEntry} */ entry) => {
       if (path.basename(entry.path) === 'uniglyph.cpp') {
-        entry.pipe(
-          concatStream((body) => {
-            /** @type {Record<string, string>} */
-            const map = {}
-            const prefix = '  { "'
-            const data = String(body)
-              .split('\n')
-              .filter((line) => line.slice(0, prefix.length) === prefix)
-              .map((line) => line.trim().replace('{', '[').replace('}', ']'))
-              .join('\n')
-            /** @type {Array<[string, string]>} */
-            const list = JSON.parse('[' + data.slice(0, -1) + ']')
-            let index = -1
-
-            while (++index < list.length) {
-              const row = list[index]
-              const characters = row[0]
-                .split('_')
-                .map((point) =>
-                  String.fromCodePoint(Number.parseInt(point, 16))
-                )
-
-              if (
-                characters.some((char) => {
-                  const cp = char.codePointAt(0)
-                  return cp !== undefined && cp >= 128
-                })
-              ) {
-                const value = characters.join('')
-
-                if (value !== row[1]) {
-                  map[value] = row[1]
-                }
-              }
-            }
-
-            fs.writeFile(
-              'index.js',
-              [
-                '/**',
-                ' * Map of non-ASCII characters to Groff commands.',
-                ' *',
-                ' * @type {Record<string, string>}',
-                ' */',
-                'export const groffEscape = ' + JSON.stringify(map, null, 2),
-                ''
-              ].join('\n'),
-              bail
-            )
-          })
-        )
+        entry.pipe(concatStream(onbody))
       }
     })
   })
   .end()
+
+/** @param {Buffer} buf */
+async function onbody(buf) {
+  /** @type {Record<string, string>} */
+  const map = {}
+  const prefix = '  { "'
+  const data = String(buf)
+    .split('\n')
+    .filter((line) => line.slice(0, prefix.length) === prefix)
+    .map((line) => line.trim().replace('{', '[').replace('}', ']'))
+    .join('\n')
+  /** @type {Array<[string, string]>} */
+  const list = JSON.parse('[' + data.slice(0, -1) + ']')
+  let index = -1
+
+  while (++index < list.length) {
+    const row = list[index]
+    const characters = row[0]
+      .split('_')
+      .map((point) => String.fromCodePoint(Number.parseInt(point, 16)))
+
+    if (
+      characters.some((char) => {
+        const cp = char.codePointAt(0)
+        return cp !== undefined && cp >= 128
+      })
+    ) {
+      const value = characters.join('')
+
+      if (value !== row[1]) {
+        map[value] = row[1]
+      }
+    }
+  }
+
+  await fs.writeFile(
+    'index.js',
+    [
+      '/**',
+      ' * Map of non-ASCII characters to Groff commands.',
+      ' *',
+      ' * @type {Record<string, string>}',
+      ' */',
+      'export const groffEscape = ' + JSON.stringify(map, null, 2),
+      ''
+    ].join('\n')
+  )
+}
